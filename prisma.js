c3fd7225e5709954d85c91e07f40abae23f05ebd
salman <salmantasdid@gmail.com>
2019-09-22 04:22:06 +0600

    comments

--- prisma.js
+++ prisma.js
@@ -9,7 +9,7 @@ prisma.query.users(null, '{id, name, email}').then((data => console.log(data)))
 
 /************** important commands (run them where docker-compose.yml located)**********/
 // 1. docker-compose up -d
-// 2. prisma deploy (whenver you change datamodel.graphql)
+// 2. prisma deploy (whenyou change datamodel.graphql, deploy them to remote heroku DB, it takes some time)
 // 3. npm run get-schema (to fetch new changes in schema / updated schema) (from package.json directory)
 // docker-compose down
 // database https://data.heroku.com/datastores/33ad949e-814b-493e-a760-bbf0ebee7a87
