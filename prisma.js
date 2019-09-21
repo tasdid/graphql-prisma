bca967e457a62cd9c8ed3fa2d10a1782a0fbe33f
salman <salmantasdid@gmail.com>
2019-09-22 03:57:34 +0600

    initial commit

---
+++ prisma.js
@@ -0,0 +1,16 @@
+import { Prisma } from 'prisma-binding'
+
+const prisma = new Prisma({
+    typeDefs: 'generated/prisma.graphql',
+    endpoint: 'http://localhost:4466'
+})
+
+prisma.query.users(null, '{id, name, email}').then((data => console.log(data)))
+
+/************** important commands (run them where docker-compose.yml located)**********/
+// 1. docker-compose up -d
+// 2. prisma deploy (whenver you change datamodel.graphql)
+// 3. npm run get-schema (to fetch new changes in schema / updated schema) (from package.json directory)
+// docker-compose down
+// database https://data.heroku.com/datastores/33ad949e-814b-493e-a760-bbf0ebee7a87
+/***************************************************************************************/
